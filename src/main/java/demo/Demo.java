6392d54ef911e2223faec85e23ee73312efc6d1c
artmeleshko <artem.meleshko@gmail.com>
2016-11-15 14:01:28 +0200

    My first commit

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Demo {
+    
+    public boolean getBool(){
+        return true;
+    } 
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
